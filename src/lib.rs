0b8f7084de77d9b660e910dc9bc9fd02013f3d54
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-01-08 23:51:15 +0800

    Move tests to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -217,3 +217,63 @@ impl DirTreeBuilder {
 fn strip_path(path: &str) -> &Path {
     Path::new(path.strip_prefix('/').unwrap_or(path))
 }
+
+#[cfg(test)]
+mod test {
+    use crate::TarFS;
+    use tempfile::tempfile;
+    use vfs::VfsPath;
+
+    #[test]
+    fn basic() {
+        let file = tempfile().unwrap();
+        let mut archive = tar_rs::Builder::new(file);
+        archive.append_dir_all("src", "src").unwrap();
+        let file = archive.into_inner().unwrap();
+
+        let fs = TarFS::from_std_file(&file).unwrap();
+        let root = VfsPath::from(fs);
+        let mut files = root
+            .join("src")
+            .unwrap()
+            .read_dir()
+            .unwrap()
+            .map(|p| p.filename())
+            .collect::<Vec<_>>();
+        files.sort();
+        assert_eq!(&files, &["lib.rs", "parser.rs"]);
+
+        let mut buffer = String::new();
+        root.join("src/lib.rs")
+            .unwrap()
+            .open_file()
+            .unwrap()
+            .read_to_string(&mut buffer)
+            .unwrap();
+        let real_content = std::fs::read_to_string("src/lib.rs").unwrap();
+        assert_eq!(buffer, real_content);
+    }
+
+    #[test]
+    fn long() {
+        let name = "a".repeat(1024);
+
+        let file = tempfile().unwrap();
+        let mut archive = tar_rs::Builder::new(file);
+        archive.append_path_with_name("src/lib.rs", &name).unwrap();
+        let file = archive.into_inner().unwrap();
+
+        let fs = TarFS::from_std_file(&file).unwrap();
+        let root = VfsPath::from(fs);
+
+        let mut buffer = String::new();
+        root.join(name)
+            .unwrap()
+            .open_file()
+            .unwrap()
+            .read_to_string(&mut buffer)
+            .unwrap();
+        let real_content = std::fs::read_to_string("src/lib.rs").unwrap();
+        assert_eq!(buffer, real_content);
+    }
+}
